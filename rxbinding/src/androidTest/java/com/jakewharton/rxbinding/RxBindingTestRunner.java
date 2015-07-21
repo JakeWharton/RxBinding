416d25990ace07f5ffda0b6d169394d7bf2c71f2
Michael Evans <michaelcevans10@gmail.com>
2015-07-20 22:58:49 -0400

    Add custom test runner to kill keyguard and hold wake lock

---
+++ rxbinding/src/androidTest/java/com/jakewharton/rxbinding/RxBindingTestRunner.java
@@ -0,0 +1,34 @@
+package com.jakewharton.rxbinding;
+
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.os.PowerManager;
+import android.support.test.runner.AndroidJUnitRunner;
+
+import static android.content.Context.KEYGUARD_SERVICE;
+import static android.content.Context.POWER_SERVICE;
+import static android.os.PowerManager.ACQUIRE_CAUSES_WAKEUP;
+import static android.os.PowerManager.FULL_WAKE_LOCK;
+import static android.os.PowerManager.ON_AFTER_RELEASE;
+
+public class RxBindingTestRunner extends AndroidJUnitRunner {
+  @Override public void onStart() {
+    runOnMainSync(new Runnable() {
+      @SuppressWarnings("deprecation") // We don't care about deprecation here.
+      @Override public void run() {
+        Context app = getTargetContext().getApplicationContext();
+
+        String name = RxBindingTestRunner.class.getSimpleName();
+        // Unlock the device so that the tests can input keystrokes.
+        KeyguardManager keyguard = (KeyguardManager) app.getSystemService(KEYGUARD_SERVICE);
+        keyguard.newKeyguardLock(name).disableKeyguard();
+        // Wake up the screen.
+        PowerManager power = (PowerManager) app.getSystemService(POWER_SERVICE);
+        power.newWakeLock(FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP | ON_AFTER_RELEASE, name)
+            .acquire();
+      }
+    });
+
+    super.onStart();
+  }
+}
