4a26f794a8b1cb6a74e81045e5f20201ea0f7790
Henri Sweers <pandanomic@gmail.com>
2015-07-27 20:49:14 -0700

    In-repo Gradle plugin for generating the Kotlin bindings.
    The 'generateKotlin' task visits the parsed Java files' AST to retrieve all the pertinent information (imports, methods, package, etc) and then generates Kotlin bindings for those methods in the sibling module.

--- rxbinding-kotlin/src/main/kotlin/com/jakewharton/rxbinding/widget/RxCompoundButton.kt
+++ rxbinding-kotlin/src/main/kotlin/com/jakewharton/rxbinding/widget/RxCompoundButton.kt
@@ -5,40 +5,43 @@ import rx.Observable
 import rx.functions.Action1
 
 /**
- * Create an observable of booleans representing the checked state of {@code view}.
+ * Create an observable of booleans representing the checked state of `view`.
  * 
- * *Warning:* The created observable keeps a strong reference to {@code view}. Unsubscribe
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
  * to free this reference.
  * 
- * *Warning:* The created observable uses {@link CompoundButton#setOnCheckedChangeListener}
+ * *Warning:* The created observable uses [CompoundButton.setOnCheckedChangeListener]
  * to observe checked changes. Only one observable can be used for a view at a time.
+ * 
+ * *Note:* A value will be emitted immediately on subscribe.
  */
 public inline fun CompoundButton.checkedChanges(): Observable<Boolean> = RxCompoundButton.checkedChanges(this)
 
 /**
- * Create an observable of checked change events for {@code view}.
+ * Create an observable of checked change events for `view`.
  * 
- * *Warning:* The created observable keeps a strong reference to {@code view}. Unsubscribe
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
  * to free this reference.
  * 
- * *Warning:* The created observable uses {@link CompoundButton#setOnCheckedChangeListener}
+ * *Warning:* The created observable uses [CompoundButton.setOnCheckedChangeListener]
  * to observe checked changes. Only one observable can be used for a view at a time.
+ * 
+ * *Note:* A value will be emitted immediately on subscribe.
  */
 public inline fun CompoundButton.checkedChangeEvents(): Observable<CompoundButtonCheckedChangeEvent> = RxCompoundButton.checkedChangeEvents(this)
 
 /**
- * An action which sets the checked property of {@code view}.
+ * An action which sets the checked property of `view`.
  * 
- * *Warning:* The created observable keeps a strong reference to {@code view}. Unsubscribe
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
  * to free this reference.
  */
 public inline fun CompoundButton.checked(): Action1<in Boolean> = RxCompoundButton.checked(this)
 
 /**
- * An action which sets the toggles property of {@code view} with each value.
+ * An action which sets the toggles property of `view` with each value.
  * 
- * *Warning:* The created observable keeps a strong reference to {@code view}. Unsubscribe
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
  * to free this reference.
  */
 public inline fun CompoundButton.toggle(): Action1<in Any> = RxCompoundButton.toggle(this)
-
