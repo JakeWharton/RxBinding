4dcef08116c0f4d3cca7b7303b80275fff606a73
Jake Wharton <jw@squareup.com>
2015-08-09 01:45:16 -0400

    Add TextSwitcher bindings.

---
+++ rxbinding-kotlin/src/main/kotlin/com/jakewharton/rxbinding/widget/RxTextSwitcher.kt
@@ -0,0 +1,21 @@
+package com.jakewharton.rxbinding.widget
+
+import android.widget.TextSwitcher
+import rx.Observable
+import rx.functions.Action1
+
+/**
+ * An action which sets the text property of `view`.
+ * 
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+public inline fun TextSwitcher.text(): Action1<in CharSequence> = RxTextSwitcher.text(this)
+
+/**
+ * An action which sets the current text property of `view`.
+ * 
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+public inline fun TextSwitcher.currentText(): Action1<in CharSequence> = RxTextSwitcher.currentText(this)
