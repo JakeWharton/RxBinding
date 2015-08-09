9027b5e60ea96d42679fcfa74b633c4b2eb4c750
Jake Wharton <jw@squareup.com>
2015-08-09 16:18:01 -0400

    Add binding for navigation view item selections.

---
+++ rxbinding-design/src/main/java/com/jakewharton/rxbinding/support/design/widget/NavigationViewItemSelectionsOnSubscribe.java
@@ -0,0 +1,50 @@
+package com.jakewharton.rxbinding.support.design.widget;
+
+import android.support.design.widget.NavigationView;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.jakewharton.rxbinding.internal.MainThreadSubscription;
+import rx.Observable;
+import rx.Subscriber;
+
+import static com.jakewharton.rxbinding.internal.Preconditions.checkUiThread;
+
+final class NavigationViewItemSelectionsOnSubscribe implements Observable.OnSubscribe<MenuItem> {
+  private final NavigationView view;
+
+  public NavigationViewItemSelectionsOnSubscribe(NavigationView view) {
+    this.view = view;
+  }
+
+  @Override public void call(final Subscriber<? super MenuItem> subscriber) {
+    checkUiThread();
+
+    NavigationView.OnNavigationItemSelectedListener listener =
+        new NavigationView.OnNavigationItemSelectedListener() {
+          @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+            if (!subscriber.isUnsubscribed()) {
+              subscriber.onNext(menuItem);
+            }
+            return true;
+          }
+        };
+
+    subscriber.add(new MainThreadSubscription() {
+      @Override protected void onUnsubscribe() {
+        view.setNavigationItemSelectedListener(null);
+      }
+    });
+
+    view.setNavigationItemSelectedListener(listener);
+
+    // Emit initial checked item, if one can be found.
+    Menu menu = view.getMenu();
+    for (int i = 0, count = menu.size(); i < count; i++) {
+      MenuItem item = menu.getItem(i);
+      if (item.isChecked()) {
+        subscriber.onNext(item);
+        break;
+      }
+    }
+  }
+}
---
+++ rxbinding-design/src/main/java/com/jakewharton/rxbinding/support/design/widget/RxNavigationView.java
@@ -0,0 +1,29 @@
+package com.jakewharton.rxbinding.support.design.widget;
+
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
+import rx.Observable;
+import rx.functions.Action1;
+
+/**
+ * Static factory methods for creating {@linkplain Observable observables} and {@linkplain Action1
+ * actions} for {@link NavigationView}.
+ */
+public final class RxNavigationView {
+  /**
+   * Create an observable which emits the selected item in {@code view}.
+   * <p>
+   * <em>Warning:</em> The created observable keeps a strong reference to {@code view}. Unsubscribe
+   * to free this reference.
+   * <p>
+   * <em>Note:</em> If an item is already selected, it will be emitted immediately on subscribe.
+   * This behavior assumes but does not enforce that the items are exclusively checkable.
+   */
+  public static Observable<MenuItem> itemSelections(NavigationView view) {
+    return Observable.create(new NavigationViewItemSelectionsOnSubscribe(view));
+  }
+
+  private RxNavigationView() {
+    throw new AssertionError("No instances.");
+  }
+}
