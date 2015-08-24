842c0ccd5ed3d93a814844c0d3cd42cc11188525
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-08-24 08:09:47 +0200

    Add RecyclerView's Adapter binding
    Fix RecyclerAdapter Tests and rename it
    
    Add Recyclerview's Adapter binding
    
    Add final to RxRecyclerViewAdapter
    
    Add final to RxRecyclerViewAdapterTest
    
    Fix indentation for RxRecyclerViewAdapter.kt
    
    two-space indentation
    
    Add imports for Adapter and ViewHolder to work with generatekotlin
    
    Run generateKotling on RxRecyclerViewAdapter
    
    Add NonNull check on return and paramater of datachanges

---
+++ rxbinding-recyclerview-v7-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v7/widget/RxRecyclerViewAdapter.kt
@@ -0,0 +1,13 @@
+package com.jakewharton.rxbinding.support.v7.widget
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.support.v7.widget.RecyclerView.ViewHolder
+import rx.Observable
+
+/**
+ * Create an observable of data change events for `RecyclerView.adapter`.
+ * 
+ * *Note:* A value will be emitted immediately on subscribe.
+ */
+public inline fun <T : Adapter<out ViewHolder>> T.dataChanges(): Observable<T> = RxRecyclerViewAdapter.dataChanges(this)
