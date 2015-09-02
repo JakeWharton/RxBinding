cd426448a0fa03ccce8bc1c7a9e2d02ed2b2244e
Fumihiro Xue <xfumihiro@gmail.com>
2015-09-02 12:42:19 +0800

    Add ViewGroup HierarchyChange bindings

---
+++ rxbinding/src/androidTest/java/com/jakewharton/rxbinding/view/RxViewGroupTestActivity.java
@@ -0,0 +1,19 @@
+package com.jakewharton.rxbinding.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public final class RxViewGroupTestActivity extends Activity {
+  ViewGroup viewGroup;
+  View child;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    viewGroup = new LinearLayout(this);
+    child = new View(this);
+    setContentView(viewGroup);
+  }
+}
---
+++ rxbinding/src/main/java/com/jakewharton/rxbinding/view/RxViewGroup.java
@@ -0,0 +1,21 @@
+package com.jakewharton.rxbinding.view;
+
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+import android.view.ViewGroup;
+import rx.Observable;
+
+public final class RxViewGroup {
+  /**
+   * Create an observable of hierarchy change events for {@code viewGroup}.
+   */
+  @CheckResult @NonNull
+  public static Observable<ViewGroupHierarchyChangeEvent> changeEvents(@NonNull ViewGroup viewGroup) {
+    return Observable.create(new ViewGroupHierarchyChangeEventOnSubscribe(viewGroup));
+  }
+
+  private RxViewGroup() {
+    throw new AssertionError("No instances.");
+  }
+
+}
