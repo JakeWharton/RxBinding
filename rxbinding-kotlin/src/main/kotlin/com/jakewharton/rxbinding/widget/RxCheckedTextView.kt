1ecab5e4c5446ce58f6b85d639dac11cb9c6fd2f
Fumihiro Xue <xfumihiro@gmail.com>
2015-10-11 21:52:32 +0800

    Add RxCheckedTextView

---
+++ rxbinding-kotlin/src/main/kotlin/com/jakewharton/rxbinding/widget/RxCheckedTextView.kt
@@ -0,0 +1,12 @@
+package com.jakewharton.rxbinding.widget
+
+import android.widget.CheckedTextView
+import rx.functions.Action1
+
+/**
+ * An action which sets the checked property of `view` with a boolean value.
+ * 
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+public inline fun CheckedTextView.check(): Action1<in Boolean> = RxCheckedTextView.check(this)
