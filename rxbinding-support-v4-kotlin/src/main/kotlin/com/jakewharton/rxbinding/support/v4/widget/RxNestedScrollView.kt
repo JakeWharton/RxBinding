36796d2bf66562141e1d2966e7d9e8ea8e6f83aa
Bartosz Galek <bartosz.galek@allegrogroup.com>
2016-05-17 11:23:27 +0200

    generated kotlin

--- rxbinding-support-v4-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v4/widget/RxNestedScrollView.kt
+++ rxbinding-support-v4-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v4/widget/RxNestedScrollView.kt
@@ -5,9 +5,9 @@ import com.jakewharton.rxbinding.view.ViewScrollChangeEvent
 import rx.Observable
 
 /**
-   * Create an observable of scroll-change events for `nestedScrollView`.
-   * <p/>
-   * *Warning:* The created observable keeps a strong reference to `nestedScrollView`.
-   * Unsubscribe to free this reference.
-   */
+ * Create an observable of scroll-change events for `nestedScrollView`.
+ * <p/>
+ * *Warning:* The created observable keeps a strong reference to `nestedScrollView`.
+ * Unsubscribe to free this reference.
+ */
 public inline fun NestedScrollView.scrollChangeEvents(): Observable<ViewScrollChangeEvent> = RxNestedScrollView.scrollChangeEvents(this)
