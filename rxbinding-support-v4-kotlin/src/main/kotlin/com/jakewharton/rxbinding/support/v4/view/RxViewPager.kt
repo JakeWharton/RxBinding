d6892de6e8da314275388078ba6b59ea547319c1
Pawel Schmidt <pxxxx.sxxxxxx@gmail.com>
2016-08-14 00:15:50 +0200

    Added generated kotlin file

--- rxbinding-support-v4-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v4/view/RxViewPager.kt
+++ rxbinding-support-v4-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v4/view/RxViewPager.kt
@@ -2,6 +2,7 @@ package com.jakewharton.rxbinding.support.v4.view
 
 import android.support.v4.view.ViewPager
 import rx.Observable
+import rx.functions.Action1
 
 /**
  * Create an observable of scroll state change events on `view`.
@@ -21,3 +22,11 @@ public inline fun ViewPager.pageScrollStateChanges(): Observable<Int> = RxViewPa
  * *Note:* A value will be emitted immediately on subscribe.
  */
 public inline fun ViewPager.pageSelections(): Observable<Int> = RxViewPager.pageSelections(this)
+
+/**
+ * An action which sets the current item of `view`.
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+public inline fun ViewPager.currentItem(): Action1<in Int> = RxViewPager.currentItem(this)
