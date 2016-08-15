c5ca269d4937f671de08654965bf4f4f2cc38596
alex-townsend <aft5014@gmail.com>
2016-08-15 11:50:26 -0400

    add new SlidingPaneLayout bindings

---
+++ rxbinding-support-v4-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/v4/widget/RxSlidingPaneLayout.kt
@@ -0,0 +1,37 @@
+package com.jakewharton.rxbinding.support.v4.widget
+
+import android.support.v4.widget.SlidingPaneLayout
+import rx.Observable
+import rx.functions.Action1
+
+/**
+ * Create an observable of the open state of the pane of `view`
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ *
+ * *Warning:* The created observable uses [SlidingPaneLayout.setPanelSlideListener]
+ * to observe dismiss change. Only one observable can be used for a view at a time.
+ *
+ * *Note:* A value will be emitted immediately on subscribe.
+ */
+public inline fun SlidingPaneLayout.paneOpen(): Observable<Boolean> = RxSlidingPaneLayout.paneOpen(this)
+
+/**
+ * Create an observable of the slide offset of the pane of `view`
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ *
+ * *Warning:* The created observable uses [SlidingPaneLayout.setPanelSlideListener]
+ * to observe dismiss change. Only one observable can be used for a view at a time.
+ */
+public inline fun SlidingPaneLayout.slides(): Observable<Float> = RxSlidingPaneLayout.slides(this)
+
+/**
+ * An action which sets whether the pane of `view` is open.
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+public inline fun SlidingPaneLayout.open(): Action1<in Boolean> = RxSlidingPaneLayout.open(this)
