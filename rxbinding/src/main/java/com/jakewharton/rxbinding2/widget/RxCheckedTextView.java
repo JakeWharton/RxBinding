94c120ae12ac98d9d34a9a45bdf4a2657cbebb88
ghostbuster91 <kghost0@gmail.com>
2016-12-08 16:15:53 +0100

    Port RxCheckedTextView

--- rxbinding/src/main/java/com/jakewharton/rxbinding2/widget/RxCheckedTextView.java
+++ rxbinding/src/main/java/com/jakewharton/rxbinding2/widget/RxCheckedTextView.java
@@ -1,28 +1,28 @@
-package com.jakewharton.rxbinding.widget;
+package com.jakewharton.rxbinding2.widget;
 
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.widget.CheckedTextView;
-import rx.functions.Action1;
+import io.reactivex.functions.Consumer;
 
 import static com.jakewharton.rxbinding.internal.Preconditions.checkNotNull;
 
 /**
- * Static factory methods for creating {@linkplain Action1 actions} for {@link CheckedTextView}.
+ * Static factory methods for creating {@linkplain Consumer actions} for {@link CheckedTextView}.
  */
 public final class RxCheckedTextView {
 
   /**
-   * An action which sets the checked property of {@code view} with a boolean value.
+   * A consumer which sets the checked property of {@code view} with a boolean value.
    * <p>
    * <em>Warning:</em> The created observable keeps a strong reference to {@code view}. Unsubscribe
    * to free this reference.
    */
   @CheckResult @NonNull
-  public static Action1<? super Boolean> check(@NonNull final CheckedTextView view) {
+  public static Consumer<? super Boolean> check(@NonNull final CheckedTextView view) {
     checkNotNull(view, "view == null");
-    return new Action1<Boolean>() {
-      @Override public void call(Boolean check) {
+    return new Consumer<Boolean>() {
+      @Override public void accept(Boolean check) {
         view.setChecked(check);
       }
     };
