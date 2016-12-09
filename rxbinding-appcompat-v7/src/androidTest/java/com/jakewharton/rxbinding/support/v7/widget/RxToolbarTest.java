3b17e60f640d6505f17ac1e601868a7fc1c452f1
Marcin Suska <marcin.suska@elpassion.pl>
2016-12-09 17:34:47 +0100

    Port RxToolbar

--- rxbinding-appcompat-v7/src/androidTest/java/com/jakewharton/rxbinding/support/v7/widget/RxToolbarTest.java
+++ rxbinding-appcompat-v7/src/androidTest/java/com/jakewharton/rxbinding/support/v7/widget/RxToolbarTest.java
@@ -8,11 +8,10 @@ import android.view.MenuItem;
 import com.jakewharton.rxbinding.RecordingObserver;
 import com.jakewharton.rxbinding.support.v7.appcompat.test.R;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -37,7 +36,7 @@ public final class RxToolbarTest {
     MenuItem item2 = menu.add(0, 2, 0, "Hey");
 
     RecordingObserver<MenuItem> o = new RecordingObserver<>();
-    Subscription subscription = RxToolbar.itemClicks(view).subscribe(o);
+    RxToolbar.itemClicks(view).subscribe(o);
     o.assertNoMoreEvents();
 
     menu.performIdentifierAction(2, 0);
@@ -46,15 +45,15 @@ public final class RxToolbarTest {
     menu.performIdentifierAction(1, 0);
     assertThat(o.takeNext()).isSameAs(item1);
 
-    subscription.unsubscribe();
+    o.dispose();
 
     menu.performIdentifierAction(2, 0);
     o.assertNoMoreEvents();
   }
 
   @Test public void navigationClicks() {
-    RecordingObserver<Void> o = new RecordingObserver<>();
-    Subscription subscription = RxToolbar.navigationClicks(view)
+    RecordingObserver<Object> o = new RecordingObserver<>();
+    RxToolbar.navigationClicks(view)
         .subscribeOn(AndroidSchedulers.mainThread())
         .subscribe(o);
     o.assertNoMoreEvents(); // No initial value.
@@ -65,29 +64,29 @@ public final class RxToolbarTest {
     onView(withContentDescription(NAVIGATION_CONTENT_DESCRIPTION)).perform(click());
     assertThat(o.takeNext()).isNull();
 
-    subscription.unsubscribe();
+    o.dispose();
 
     onView(withContentDescription(NAVIGATION_CONTENT_DESCRIPTION)).perform(click());
     o.assertNoMoreEvents();
   }
 
-  @Test @UiThreadTest public void title() {
-    RxToolbar.title(view).call("Hey");
+  @Test @UiThreadTest public void title() throws Exception {
+    RxToolbar.title(view).accept("Hey");
     assertThat(view.getTitle().toString()).isEqualTo("Hey");
   }
 
-  @Test @UiThreadTest public void titleRes() {
-    RxToolbar.titleRes(view).call(R.string.hey);
+  @Test @UiThreadTest public void titleRes() throws Exception {
+    RxToolbar.titleRes(view).accept(R.string.hey);
     assertThat(view.getTitle().toString()).isEqualTo("Hey");
   }
 
-  @Test @UiThreadTest public void subtitle() {
-    RxToolbar.subtitle(view).call("Hey");
+  @Test @UiThreadTest public void subtitle() throws Exception {
+    RxToolbar.subtitle(view).accept("Hey");
     assertThat(view.getSubtitle().toString()).isEqualTo("Hey");
   }
 
-  @Test @UiThreadTest public void subtitleRes() {
-    RxToolbar.subtitleRes(view).call(R.string.hey);
+  @Test @UiThreadTest public void subtitleRes() throws Exception {
+    RxToolbar.subtitleRes(view).accept(R.string.hey);
     assertThat(view.getSubtitle().toString()).isEqualTo("Hey");
   }
 }
