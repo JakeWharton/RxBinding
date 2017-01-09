5253a0798581d39ae85413813af2b15579af9afd
Alberto Ballano <alberto.ballano@xing.com>
2017-01-09 20:41:20 +0100

    Executed generateKotlin

--- rxbinding-design-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/design/widget/RxSnackbar.kt
+++ rxbinding-design-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/design/widget/RxSnackbar.kt
@@ -10,3 +10,19 @@ import rx.Observable
  * to free this reference.
  */
 inline fun Snackbar.dismisses(): Observable<Int> = RxSnackbar.dismisses(this)
+
+/**
+ * Create an observable which emits the action clicked events from `view`.
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+inline fun Snackbar.actionClicked(resId: Int): Observable<Int> = RxSnackbar.actionClicked(this, resId)
+
+/**
+ * Create an observable which emits the action clicked events from `view`.
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+inline fun Snackbar.actionClicked(text: CharSequence): Observable<Int> = RxSnackbar.actionClicked(this, text)
