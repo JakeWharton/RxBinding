c24cbf7efe172c43661af42684c51cd7d911f7ec
Karol Kowalski <ordon1313@gmail.com>
2017-09-26 01:33:27 +0200

    Remove name from KMethod

--- buildSrc/src/main/kotlin/com/jakewharton/rxbinding/project/KMethod.kt
+++ buildSrc/src/main/kotlin/com/jakewharton/rxbinding/project/KMethod.kt
@@ -14,7 +14,6 @@ import com.squareup.kotlinpoet.*
  */
 class KMethod(n: MethodDeclaration,
               private val associatedImports: Map<String, ClassName>) {
-  val name = n.name
   val annotations: List<AnnotationExpr> = n.annotations
   val comment = n.comment?.content?.let { cleanUpDoc(it) }
   val extendedClass = KotlinTypeResolver.resolveKotlinType(n.parameters[0].type,
@@ -91,43 +90,41 @@ class KMethod(n: MethodDeclaration,
 
 }
 
-fun MethodDeclaration.toFunSpec(associatedImports: Map<String, ClassName>, bindingClass: String) =
-    KMethod(this, associatedImports).generate(ClassName.bestGuess(bindingClass))
-
 /**
  * Generates the kotlin code for this method
  *
  * @param bindingClass name of the RxBinding class this is tied to
  */
-fun KMethod.generate(bindingClass: TypeName): FunSpec {
-  ///////////////
-  // STRUCTURE //
-  ///////////////
-  // Javadoc
-  // public inline fun DrawerLayout.drawerOpen(): Observable<Boolean> = RxDrawerLayout.drawerOpen(this)
-  // <access specifier> inline fun <extendedClass>.<name>(params): <type> = <bindingClass>.name(this, params)
-
-  val parameterSpecs = kParams()
-  return FunSpec.builder(name)
-      .receiver(extendedClass)
-      .addKdoc(comment ?: "")
-      .addModifiers(KModifier.INLINE)
-      .apply {
-        typeParameters?.let { addTypeVariables(it) }
-      }
-      .returns(kotlinType)
-      .addParameters(parameterSpecs)
-      .addCode("return %T.$name(${if (parameterSpecs.isNotEmpty()) {
-        "this, ${parameterSpecs.joinToString { it.name }}"
-      } else {
-        "this"
-      }})", bindingClass)
-      .apply {
-        // Object --> Unit mapping
-        if (emitsUnit()) {
-          addCode(".map(VoidToUnit)")
+fun MethodDeclaration.toFunSpec(associatedImports: Map<String, ClassName>, bindingClassName: String): FunSpec {
+  val m = this
+  return KMethod(this, associatedImports).run {
+    ///////////////
+    // STRUCTURE //
+    ///////////////
+    // Javadoc
+    // public inline fun DrawerLayout.drawerOpen(): Observable<Boolean> = RxDrawerLayout.drawerOpen(this)
+    // <access specifier> inline fun <extendedClass>.<name>(params): <type> = <bindingClass>.name(this, params)
+    FunSpec.builder(m.name)
+        .receiver(extendedClass)
+        .addKdoc(comment ?: "")
+        .addModifiers(KModifier.INLINE)
+        .apply {
+          typeParameters?.let { addTypeVariables(it) }
         }
-      }
-      .addCode("\n")
-      .build()
+        .returns(kotlinType)
+        .addParameters(kParams())
+        .addCode("return %T.$name(${if (kParams().isNotEmpty()) {
+          "this, ${kParams().joinToString { it.name }}"
+        } else {
+          "this"
+        }})", ClassName.bestGuess(bindingClassName))
+        .apply {
+          // Object --> Unit mapping
+          if (emitsUnit()) {
+            addCode(".map(VoidToUnit)")
+          }
+        }
+        .addCode("\n")
+        .build()
+  }
 }
