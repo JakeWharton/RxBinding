69f7207bd99b30df88f7aa62687e08b224ceef75
ghostbuster91 <kghost0@gmail.com>
2017-10-16 23:20:30 +0200

    Regenerate kotlin files

---
+++ rxbinding-design-kotlin/src/main/kotlin/com/jakewharton/rxbinding/support/design/widget/RxFloatingActionButton.kt
@@ -0,0 +1,17 @@
+@file:Suppress(
+    names = "NOTHING_TO_INLINE"
+)
+
+package com.jakewharton.rxbinding.support.design.widget
+
+import android.support.design.widget.FloatingActionButton
+import io.reactivex.functions.Consumer
+import kotlin.Suppress
+
+/**
+ * An action which tells the `floatingActionButton`. to show on `true` and hide on `false`
+ *
+ * *Warning:* The created observable keeps a strong reference to `view`. Unsubscribe
+ * to free this reference.
+ */
+inline fun FloatingActionButton.fabVisibility(): Consumer<in Boolean> = RxFloatingActionButton.fabVisibility(this)
