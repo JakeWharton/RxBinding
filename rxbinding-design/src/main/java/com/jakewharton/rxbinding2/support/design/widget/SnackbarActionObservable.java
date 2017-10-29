7a612f6dfbf9d95951b940affe588130156128be
Alexander Mironov <alexander.mironoff@gmail.com>
2017-10-29 13:29:06 +0300

    Add Snackbar action observables

---
+++ rxbinding-design/src/main/java/com/jakewharton/rxbinding2/support/design/widget/SnackbarActionObservable.java
@@ -0,0 +1,73 @@
+package com.jakewharton.rxbinding2.support.design.widget;
+
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.view.View.OnClickListener;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.MainThreadDisposable;
+
+import static com.jakewharton.rxbinding2.internal.Preconditions.checkMainThread;
+
+final class SnackbarActionObservable extends Observable<View> {
+  private final Snackbar view;
+  private final int resId;
+  private final CharSequence text;
+
+  SnackbarActionObservable(Snackbar view, int resId) {
+    this.view = view;
+    this.text = null;
+    this.resId = resId;
+  }
+
+  SnackbarActionObservable(Snackbar view, CharSequence text) {
+    this.view = view;
+    this.text = text;
+    this.resId = -1;
+  }
+
+  @Override protected void subscribeActual(Observer<? super View> observer) {
+    if (!checkMainThread(observer)) {
+      return;
+    }
+    Listener listener = new Listener(view, observer);
+    observer.onSubscribe(listener);
+    if (text == null) {
+      view.setAction(resId, listener.callback);
+    } else {
+      view.setAction(text, listener.callback);
+    }
+  }
+
+  final class Listener extends MainThreadDisposable {
+    private final Snackbar snackbar;
+    private final OnClickListener callback;
+
+    Listener(Snackbar snackbar, final Observer<? super View> observer) {
+      this.snackbar = snackbar;
+      this.callback = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          if (!isDisposed()) {
+            observer.onNext(view);
+          }
+        }
+      };
+    }
+
+    @Override protected void onDispose() {
+      // Provide stub OnClickListener implementation
+      // This is necessary because Snackbar will hide Action text if setAction is called with listener set to null
+      if (text == null) {
+        snackbar.setAction(resId, new EmptyActionListener());
+      } else {
+        snackbar.setAction(text, new EmptyActionListener());
+      }
+    }
+  }
+
+  private static final class EmptyActionListener implements OnClickListener {
+    @Override public void onClick(View v) {
+    }
+  }
+}
