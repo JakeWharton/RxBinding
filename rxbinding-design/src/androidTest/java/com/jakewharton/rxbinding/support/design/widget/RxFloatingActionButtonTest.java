9fb75f5d5c1041f89e9f03827731c877fc90fe0f
Karol Kowalski <ordon1313@gmail.com>
2017-11-01 13:01:44 +0100

    Wrap context with theme Fix test

--- rxbinding-design/src/androidTest/java/com/jakewharton/rxbinding/support/design/widget/RxFloatingActionButtonTest.java
+++ rxbinding-design/src/androidTest/java/com/jakewharton/rxbinding/support/design/widget/RxFloatingActionButtonTest.java
@@ -5,7 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+
+import com.jakewharton.rxbinding2.support.design.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +20,8 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class RxFloatingActionButtonTest {
 
-  private final Context context = InstrumentationRegistry.getContext();
+  private final Context rawContext = InstrumentationRegistry.getContext();
+  private final Context context = new ContextThemeWrapper(rawContext, R.style.Theme_AppCompat);
   private final FloatingActionButton fab = new FloatingActionButton(context);
 
   @Test
