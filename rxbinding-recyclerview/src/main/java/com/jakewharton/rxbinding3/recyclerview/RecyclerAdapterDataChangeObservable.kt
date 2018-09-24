cd89c49e580be48dcdc1c47404006f95bbcc2f93
Jake Wharton <jakew@google.com>
2018-09-24 12:20:56 -0400

    Port RecyclerView module to AndroidX and Kotlin

---
+++ rxbinding-recyclerview/src/main/java/com/jakewharton/rxbinding3/recyclerview/RecyclerAdapterDataChangeObservable.kt
@@ -0,0 +1,57 @@
+@file:JvmMultifileClass
+@file:JvmName("RxRecyclerViewAdapter")
+
+package com.jakewharton.rxbinding3.recyclerview
+
+import androidx.annotation.CheckResult
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.jakewharton.rxbinding2.InitialValueObservable
+import com.jakewharton.rxbinding2.internal.Preconditions.checkMainThread
+import io.reactivex.Observer
+import io.reactivex.android.MainThreadDisposable
+
+/**
+ * Create an observable of data change events for `RecyclerView.adapter`.
+ *
+ * *Note:* A value will be emitted immediately on subscribe.
+ */
+@CheckResult
+fun <T : Adapter<out ViewHolder>> T.dataChanges(): InitialValueObservable<T> =
+    RecyclerAdapterDataChangeObservable(this)
+
+private class RecyclerAdapterDataChangeObservable<T : Adapter<out ViewHolder>>(
+  private val adapter: T
+) : InitialValueObservable<T>() {
+
+  override fun subscribeListener(observer: Observer<in T>) {
+    if (!checkMainThread(observer)) {
+      return
+    }
+    val listener = Listener(
+        adapter, observer)
+    observer.onSubscribe(listener)
+    adapter.registerAdapterDataObserver(listener.dataObserver)
+  }
+
+  override fun getInitialValue() = adapter
+
+  class Listener<T : Adapter<out ViewHolder>>(
+    private val recyclerAdapter: T,
+    observer: Observer<in T>
+  ) : MainThreadDisposable() {
+
+    val dataObserver = object : AdapterDataObserver() {
+      override fun onChanged() {
+        if (!isDisposed) {
+          observer.onNext(recyclerAdapter)
+        }
+      }
+    }
+
+    override fun onDispose() {
+      recyclerAdapter.unregisterAdapterDataObserver(dataObserver)
+    }
+  }
+}
---
+++ rxbinding-recyclerview/src/main/java/com/jakewharton/rxbinding3/recyclerview/RecyclerViewFlingEventObservable.kt
@@ -0,0 +1,64 @@
+@file:JvmMultifileClass
+@file:JvmName("RxRecyclerView")
+
+package com.jakewharton.rxbinding3.recyclerview
+
+import android.content.Context
+import androidx.annotation.CheckResult
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.MainThreadDisposable
+
+import com.jakewharton.rxbinding2.internal.Preconditions.checkMainThread
+
+/**
+ * Create an observable of scroll events on `recyclerView`.
+ *
+ * *Warning:* The created observable keeps a strong reference to `recyclerView`.
+ * Unsubscribe to free this reference.
+ */
+@CheckResult
+fun RecyclerView.flingEvents(): Observable<RecyclerViewFlingEvent> =
+    RecyclerViewFlingEventObservable(this)
+
+/**
+ * A fling event on a recyclerView.
+ *
+ * **Warning:** Instances keep a strong reference to the recyclerView. Operators that
+ * cache instances have the potential to leak the associated [Context].
+ */
+data class RecyclerViewFlingEvent(val view: RecyclerView, val velocityX: Int, val velocityY: Int)
+
+private class RecyclerViewFlingEventObservable(
+  private val view: RecyclerView
+) : Observable<RecyclerViewFlingEvent>() {
+
+  override fun subscribeActual(observer: Observer<in RecyclerViewFlingEvent>) {
+    if (!checkMainThread(observer)) {
+      return
+    }
+    val listener = Listener(view, observer)
+    observer.onSubscribe(listener)
+    view.onFlingListener = listener.scrollListener
+  }
+
+  class Listener(
+    private val recyclerView: RecyclerView,
+    observer: Observer<in RecyclerViewFlingEvent>
+  ) : MainThreadDisposable() {
+
+    val scrollListener = object : RecyclerView.OnFlingListener() {
+      override fun onFling(velocityX: Int, velocityY: Int): Boolean {
+        if (!isDisposed) {
+          observer.onNext(RecyclerViewFlingEvent(recyclerView, velocityX, velocityY))
+        }
+        return false
+      }
+    }
+
+    override fun onDispose() {
+      recyclerView.onFlingListener = null
+    }
+  }
+}
